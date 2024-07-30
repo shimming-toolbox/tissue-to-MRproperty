6892c501292dcbf83786f8c23569937f75c633b1
Sebastian Rios <sebas.riosq99@gmail.com>
2024-07-29 21:25:32 -0400

    wip

--- functions/volume.py
+++ functions/volume.py
@@ -252,7 +252,9 @@ class volume:
                         if rem == "n":
                             print("Maybe you want to change the value?")
                             rem2 = int(input("If so, choose the value: "))
-
+                            print("Changed value to: ", rem2)
+                            self.volume[i, j, k] = rem2
+                            
                             if rem2 in self.look_up.keys():
                                 print("Changed value to: ", rem2)
                                 self.volume[i, j, k] = rem2
